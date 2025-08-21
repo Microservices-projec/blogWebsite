503ce21fc47104299af9047f533658cb181cf2c1
thetanish1 <tanishdewase25@gmail.com>
2025-08-21 22:51:42 +0530

    [public] added code in html page

--- routes/routes.js
+++ routes/routes.js
@@ -1,18 +1,20 @@
-const express = require("express");
-const router = express.Router();
-
-const app = express();
+const express = require("express");  // initialize expree
+const router = express.Router();  //  for enable routing 
+const path = require("path");   // for path setting 
 
 router.get("/", (req, res) => {
-  res.send(" Welcome to our Website");
+  //res.send(" Welcome to our Website"); // for normal message
+  res.sendFile(path.join(__dirname, "../public/home.html"));
 });
 
 router.get("/create", (req, res) => {
-  res.send(" Welcome to Create Blog page");
+  //res.send(" Welcome to Create Blog page"); // for normal message
+  res.sendFile(path.join(__dirname,"../public/create.html"))
 });
 
 router.get("/blogs", (req, res) => {
-  res.send(" Welcome to All Blogs page ");
+  //res.send(" Welcome to All Blogs page ");  // for normal message
+  res.sendFile(path.join(__dirname,"../public/blogs.html"))
 });
 
 
--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const app = express();
 const routes = require("./routes/routes");
+const path = require("path");
 
 app.use(express.json());
 
 // Routes use karo
 app.use("/", routes);
 
+//static file dene ke liye
+app.use(express.static(path.join(__dirname, "public")));
+
 app.listen(3000, () => {
-  console.log('Server is running at http://localhost:3000');
+  console.log("Server is running at http://localhost:3000");
 });
