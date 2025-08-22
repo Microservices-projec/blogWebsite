a368d0c498ac1b46be67c1bc77ffee2126c257bc
thetanish1 <tanishdewase25@gmail.com>
2025-08-22 07:22:53 +0530

    [routes done]

--- routes/routes.js
+++ routes/routes.js
@@ -1,21 +1,12 @@
 const express = require("express");  // initialize expree
-const router = express.Router();  //  for enable routing 
-const path = require("path");   // for path setting 
+const router = express.Router();  //  for enable routing   
+const blogController = require("../controllers/blogController");
 
-router.get("/", (req, res) => {
-  //res.send(" Welcome to our Website"); // for normal message
-  res.sendFile(path.join(__dirname, "../public/home.html"));
-});
-
-router.get("/create", (req, res) => {
-  //res.send(" Welcome to Create Blog page"); // for normal message
-  res.sendFile(path.join(__dirname,"../public/create.html"))
-});
-
-router.get("/blogs", (req, res) => {
-  //res.send(" Welcome to All Blogs page ");  // for normal message
-  res.sendFile(path.join(__dirname,"../public/blogs.html"))
-});
+router.get("/", blogController.getblogs);
+router.get("/:id", blogController.getblogById);
+router.post("/", blogController.createblog);
+router.put("/:id", blogController.updateblog);
+router.delete("/:id", blogController.deleteblog);
 
 
 module.exports = router;
